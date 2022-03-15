9d0fcb049dfe15e37bf543e7825ec65764cd3efb
zipho-binta <zipho.binta@investec.co.za>
2022-03-15 17:15:23 +0200

    init commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  appId: '691b08daafeea633a92d52473814d8c8',
+  baseUrl: 'https://'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  appId: '691b08daafeea633a92d52473814d8c8',
+  baseUrl: 'http://'
+};
+
